882b1ba738cf8117f9ea34dfad7276844d73c22a
Daniel Chiu <1360874697@qq.com>
2016-05-12 15:29:57 +0800

    2cd commit

---
+++ src/main/java/daniel/test/Test.java
@@ -0,0 +1,11 @@
+package daniel.test;
+
+/**
+ * Created by Daniel Chiu on 2016/5/12.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test based on Jenkins");
+    }
+}
